c6fbb6bc3f26c63026e965fe610da7de49cd82f6
Shashank0606 <shashankrathore606@gmail.com>
2022-11-28 21:58:43 +0530

    REFACTOR: Three Floats to Find the Maximum

---
+++ generics/src/main/java/Generics.java
@@ -0,0 +1,19 @@
+public class Generics {
+
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+		T max = x;
+
+		if (y.compareTo(max) > 0)
+			max = y;
+
+		if (z.compareTo(max) > 0)
+			max = z;
+
+		return max;
+	}
+
+	public static void main(String args[]) {
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
+				8.8, 7.7));
+	}
+}
